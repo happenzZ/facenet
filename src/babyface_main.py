42f81047888bde61d5906223ed674fd361f0b934
HaipengZeng <tsanghaipeng@gmail.com>
2017-05-13 08:41:52 +0800

    start to deal with video, initial KNN

---
+++ src/babyface_main.py
@@ -0,0 +1,401 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import sys
+import sugartensor as tf
+import numpy as np
+import importlib
+import argparse
+import facenet
+import align.detect_face
+from six.moves import xrange
+from scipy import misc
+import cv2
+import imutils
+import json
+from sklearn.neighbors import NearestNeighbors
+from matplotlib import pyplot as plt
+
+print('Creating networks and loading parameters')
+with tf.Graph().as_default():
+    # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+    # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    sess = tf.Session()
+    with sess.as_default():
+        pnet, rnet, onet = align.detect_face.create_mtcnn(sess, './align/')
+
+network = importlib.import_module('models.inception_resnet_v1', 'inference')
+
+
+def save_image(img, faceRectangles, imgPath):
+    # # Draw a rectangle around the faces
+    for faceIdx, (left, top, right, bottom, p) in enumerate(faceRectangles):
+        cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, 'faceIdx: {}'.format(faceIdx), (int(left), int(top)), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+    misc.imsave(imgPath, img)
+    return
+
+
+def detect_face(img):
+    # detect face bounding boxes from each frame
+    minsize = 20  # minimum size of face
+    threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+    factor = 0.709  # scale factor
+
+    bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+
+    return bounding_boxes
+
+
+def crop_face(img, bounding_boxes, image_size, margin):
+    # crop face from each frame based on bounding boxed detected
+    img_size = np.asarray(img.shape)[0:2]
+    nrof_faces = bounding_boxes.shape[0]
+    crop_images = []
+    prewhitened_images = []
+    if nrof_faces > 0:
+        for faceIdx in xrange(nrof_faces):
+            det = np.squeeze(bounding_boxes[faceIdx, 0:4])
+            bb = np.zeros(4, dtype=np.int32)
+            bb[0] = np.maximum(det[0] - margin / 2, 0)
+            bb[1] = np.maximum(det[1] - margin / 2, 0)
+            bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
+            bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
+            cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+            aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+            prewhitened = facenet.prewhiten(aligned)
+            crop_images.append(aligned)
+            prewhitened_images.append(prewhitened)
+    return crop_images, prewhitened_images
+
+
+def recognize_face(images, args):
+    np.random.seed(seed=args.seed)
+    g = tf.Graph()
+    with g.as_default():
+        # Placeholder for input images
+        images_placeholder = tf.placeholder(tf.float32, shape=(1, args.image_size, args.image_size, 3), name='input')
+
+        # Build the inference graph
+        prelogits, _ = network.inference(images_placeholder, 1.0, phase_train=False, weight_decay=0.0)
+        # embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
+
+        # Create list with variables to restore
+        # restore_vars = []
+        # for var in tf.global_variables():
+        #     if 'InceptionResnetV1/' in var.op.name:
+        #         restore_vars.append(var)
+        # restore_saver = tf.train.Saver(restore_vars)
+
+        train_set = facenet.get_dataset('../datasets/lfw_mtcnnpy_160')
+        nrof_classes = len(train_set)
+        print('Total number of classes: %d' % nrof_classes)
+
+        with tf.sg_context(name='classifier'):
+            logits = prelogits.sg_dense(dim=nrof_classes, bias=True, act='softmax')
+
+        max_index = tf.argmax(logits, axis=1)
+        confidence = tf.reduce_max(logits, axis=1)
+
+        sess = tf.Session()
+        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+        # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
+
+        tf.train.start_queue_runners(sess=sess)
+
+        with sess.as_default():
+            saver = tf.train.Saver()
+            print(tf.train.latest_checkpoint('asset/train'))
+            saver.restore(sess, tf.train.latest_checkpoint('asset/train'))
+
+            feed_dict = {images_placeholder: images}
+            print('Start calculating result')
+            max_ind, coin = sess.run([max_index, confidence], feed_dict=feed_dict)
+            # for faceIdex in xrange(len(images)):
+            #     print('Result: %s' % train_set[max_ind].name)
+            #     print('Score: %f' % coin)
+
+
+def variance_of_laplacian(image):
+    # compute the Laplacian of the image and then return the focus
+    #  measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+
+def get_image_paths_and_labels(dataset):
+    mapping_label2ImgName = []
+    image_paths_flat = []
+    labels_flat = []
+    for i in range(len(dataset)):
+        image_paths_flat += dataset[i].image_paths
+        labels_flat += [i] * len(dataset[i].image_paths)
+        mapping_label2ImgName.append(dataset[i].name)
+    return image_paths_flat, labels_flat, mapping_label2ImgName
+
+
+def process_frame(frame, image_size, margin):
+    print('process_frame')
+    bounding_boxes = detect_face(frame)
+    crop_images = crop_face(frame, bounding_boxes, image_size, margin)
+    print('len(crop_images): ', len(crop_images))
+    return
+
+
+def main(args):
+    #    with tf.Graph().as_default():
+    tf.set_random_seed(args.seed)
+    g = tf.Graph()
+    with g.as_default():
+
+        # Placeholder for input images
+        images_placeholder = tf.placeholder(tf.float32, shape=(None, args.image_size, args.image_size, 3),
+                                            name='input')
+
+        # Build the inference graph
+        prelogits, _ = network.inference(images_placeholder, 1.0,
+                                         phase_train=False, weight_decay=0.0)
+        embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
+
+        sess = tf.Session()
+        #    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+        #    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        sess.run(tf.group(tf.global_variables_initializer(),
+                          tf.local_variables_initializer()))
+
+        with sess.as_default():
+            saver = tf.train.Saver()
+            print(tf.train.latest_checkpoint('asset/train'))
+            saver.restore(sess, tf.train.latest_checkpoint('asset/train'))
+
+            # read from image database
+            train_set = facenet.get_dataset('../videos/result')
+
+            if not train_set:
+                image_embeddings = []
+                image_labels = []
+            else:
+                with open('../videos/result/image_embeddings.json', 'r') as rf:
+                    image_embeddings = json.load(rf)
+                with open('../videos/result/image_labels.json', 'r') as rf:
+                    image_labels = json.load(rf)
+
+            image_embeddings = np.array(image_embeddings)
+            image_labels = np.array(image_labels)
+
+            videoPath = '../videos/raw/babyScene_1.MOV'
+            capture = cv2.VideoCapture(videoPath)
+
+            if not capture.isOpened():
+                print("could not open :", videoPath)
+            else:
+                totalFrame = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+                width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+                height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+                fps = capture.get(cv2.CAP_PROP_FPS)
+
+                videoFormat = capture.get(cv2.CAP_PROP_FORMAT)
+
+                print('totalFrame: ', totalFrame)
+                print('width: ', width)
+                print('height: ', height)
+                print('fps: ', fps)
+                print('videoFormat: ', videoFormat)
+
+            frameIdx = 0
+            while True:
+                frameIdx += 1
+                print('frameIdx: ', frameIdx)
+                # grab the current frame
+                (grabbed, frame) = capture.read()
+
+                # if we are viewing a video and we did not grab a frame,
+                # then we have reached the end of the video
+                if not grabbed:
+                    break
+
+                if frameIdx % 30 != 0:
+                    continue
+
+                if frameIdx > 400:
+                    break
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                fm = variance_of_laplacian(gray)
+
+                # detect blur
+                if fm < 400:
+                    continue
+
+                # fm = variance_of_laplacian(gray)
+                # text = "Not Blurry"
+                # threshold = 400
+                #
+                # if fm < threshold:
+                #     text = "Blurry"
+                #
+                # # show the image
+                # cv2.putText(frame, "{}: {:.2f}".format(text, fm), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+                #
+                # cropImgPath = '../videos/blur'
+                # if not os.path.exists(cropImgPath):
+                #     os.makedirs(cropImgPath)
+                # misc.imsave(cropImgPath+'/{}.jpg'.format(frameIdx), cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+                bounding_boxes = detect_face(frame)
+                crop_images, prewhitened_images = crop_face(frame, bounding_boxes, args.image_size, args.margin)
+                print('len(crop_images): ', len(crop_images))
+
+                feed_dict = {images_placeholder: prewhitened_images}
+                print('Start calculating result')
+                tmp_embeddings = sess.run(embeddings, feed_dict=feed_dict)
+                print('img_embeddings.shape: ', tmp_embeddings.shape)
+
+                print('img_embeddings: ', tmp_embeddings)
+
+
+                imgPath  = '../videos/result'
+                if not train_set:
+                    print('not image_embeddings')
+                    print('image_embeddings: ',  image_embeddings)
+                    for idx, cropImg in enumerate(crop_images):
+                        cropImgPath = imgPath+'/{}'.format(idx)
+                        if not os.path.exists(cropImgPath):
+                            os.makedirs(cropImgPath)
+                        misc.imsave(cropImgPath+'/{}.png'.format(0), cropImg)
+
+
+                    image_embeddings = tmp_embeddings
+                    image_labels = np.array(range(len(image_embeddings)))
+                else:
+                    # image_embeddings = image_embeddings.astype(np.float32)
+                    # image_labels = image_labels.astype(np.float32)
+                    # tmp_embeddings = tmp_embeddings.astype(np.float32)
+                    # print('knn')
+                    # knn = cv2.ml.KNearest_create()
+                    # knn.train(image_embeddings, cv2.ml.ROW_SAMPLE, image_labels)
+                    # ret, results, neighbours, dist = knn.findNearest(tmp_embeddings, 2)
+                    #
+                    # print('ret: ', ret)
+                    # print('results: ', results)
+                    # print('neighbours: ', neighbours)
+                    # print('dist: ', dist)
+
+                    nbrs = NearestNeighbors(n_neighbors=2, algorithm='auto').fit(image_embeddings)
+                    distances, indices = nbrs.kneighbors(tmp_embeddings)
+
+                    print('distances: ', distances)
+                    print('indices: ', indices)
+
+                # imgPath = '../videos/test_30'
+                # if not os.path.exists(imgPath):
+                #     os.makedirs(imgPath)
+                # save_image(frame, bounding_boxes, imgPath+'/{}.jpg'.format(frameIdx))
+                #
+                # for idx, cropImg in enumerate(crop_images):
+                #     cropImgPath = imgPath+'/{}'.format(frameIdx)
+                #     if not os.path.exists(cropImgPath):
+                #         os.makedirs(cropImgPath)
+                #     misc.imsave(cropImgPath+'/{}.jpg'.format(idx), cropImg)
+
+                frame = imutils.resize(frame, width=600)
+                cv2.imshow('frame', frame)
+                ck = cv2.waitKey(30) & 0xFF
+                if ck == 27:
+                    break
+
+            print('image_embeddings.shape: ', image_embeddings.shape)
+            print('image_labels.shape: ', image_labels.shape)
+
+            with open('../videos/result/image_embeddings.json', 'w') as wf:
+                json.dump(image_embeddings.tolist(), wf)
+
+            with open('../videos/result/image_labels.json', 'w') as wf:
+                json.dump(image_labels.tolist(), wf)
+
+            capture.release()
+            cv2.destroyAllWindows()
+            print('hello world')
+
+
+
+            # feed_dict = {images_placeholder: image}
+            # print('Start calculating result')
+            # max_ind, coin, prelog, embed = sess.run([max_index, confidence, prelogits, embeddings], feed_dict=feed_dict)
+            # for faceIdx in xrange(len(image)):
+            #     print('faceID: {}, Result: {}'.format(faceIdx, train_set[max_ind[faceIdx]].name))
+            #     print('faceID: {}, Score: {}'.format(faceIdx, coin[faceIdx]))
+            #
+            # print('prelogits.shape: ', prelogits.shape)
+            # print('embeddings.shape: ', embeddings.shape)
+
+
+def load_and_align_data(image_paths, image_size, margin):
+    minsize = 20  # minimum size of face
+    threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+    factor = 0.709  # scale factor
+
+    # image format: RGB
+    img = misc.imread(os.path.expanduser(image_paths))
+    print('type(img)', type(img))
+    print('img.shape', img.shape)
+    print("load_and_align_data.img.shape: ", img.shape)
+    img_size = np.asarray(img.shape)[0:2]
+    bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+    nrof_faces = bounding_boxes.shape[0]
+
+    print('nrof_faces: ', nrof_faces)
+
+    # plt.imshow(img)
+    # plt.show()
+    save_image(img, bounding_boxes)
+
+    if nrof_faces > 0:
+        crop_images = []
+        for faceIdx in xrange(nrof_faces):
+            det = np.squeeze(bounding_boxes[faceIdx, 0:4])
+            bb = np.zeros(4, dtype=np.int32)
+            bb[0] = np.maximum(det[0] - margin / 2, 0)
+            bb[1] = np.maximum(det[1] - margin / 2, 0)
+            bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
+            bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
+            cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+            aligned = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+            prewhitened = facenet.prewhiten(aligned)
+            crop_images.append(prewhitened)
+    else:
+        crop_images = None
+
+    return crop_images
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_file', type=str, help='Images to compare', default='../faceDataset/test/lfw_test.jpg')
+    # parser.add_argument('--image_file', type=str, help='Images to compare', default='../faceDataset/test/babyImage.jpg')
+    parser.add_argument('--gpu_memory_fraction', type=float,
+                        help='Upper bound on the amount of GPU memory that will be used by the process.', default=0.67)
+    parser.add_argument('--pretrained_model', type=str,
+                        help='Load a pretrained model before training starts.',
+                        default='../preModels/20161116-234200/model-20161116-234200.ckpt-80000')
+    parser.add_argument('--image_size', type=int,
+                        help='Image size (height, width) in pixels.', default=160)
+    parser.add_argument('--random_crop',
+                        help='Performs random cropping of training images. If false, the center image_size pixels from the training images are used. ' +
+                             'If the size of the images in the data directory is equal to image_size no cropping is performed',
+                        action='store_true')
+    parser.add_argument('--seed', type=int,
+                        help='Random seed.', default=666)
+    parser.add_argument('--margin', type=int,
+                        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
