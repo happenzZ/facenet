96ac00f6d18e3b276e9a4dcd6a3d305fc9e82710
HaipengZeng <tsanghaipeng@gmail.com>
2017-05-13 22:47:05 +0800

    some test

--- src/test.py
+++ src/test.py
@@ -1,82 +1,169 @@
-import cv2
-from scipy import misc
-import imutils
+import numpy as np
+
+dist = np.array([3.3, 5.2, 9.7])
+ind = np.array([2, 4, 1])
+image_labels = np.array(['a', 'b', 'b', 'd', 'e'])
 
 
-def save_image(img, faceRectangles, imgPath):
-    # # Draw a rectangle around the faces
-    for faceIdx, (left, top, right, bottom, p) in enumerate(faceRectangles):
-        cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2)
+def knn_judge(dist, ind, image_labels):
+    label_distIdx = {}
+    label_count = []
+    label_list = []   # just record label
+    for idx in range(ind.shape[0]):
+        label = image_labels[ind[idx]]
+        if label not in label_distIdx:
+            label_distIdx[label] = []
+        label_distIdx[label].append(idx)
+        if label not in label_list:
+            label_list.append(label)
+    for label in label_list:
+        label_count.append([label, len(label_distIdx[label])])
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, 'faceIdx: {}'.format(faceIdx), (int(left), int(top)), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
-    misc.imsave(imgPath, img)
-    return
+    print(sorted(label_count, key=lambda x: x[1], reverse=True))
 
+    print('label_distIdx: ', label_distIdx)
+    print('label_count: ', label_count)
+    print('label_list: ', label_list)
 
-def variance_of_laplacian(image):
-    # compute the Laplacian of the image and then return the focus
-    #  measure, which is simply the variance of the Laplacian
-    return cv2.Laplacian(image, cv2.CV_64F).var()
+    label = sorted(label_count, key=lambda x: x[1], reverse=True)[0][0]
+    distance = np.mean(dist[label_distIdx[label]])
 
+    print('label: ', label)
+    print(label_distIdx[label])
+    print(dist[label_distIdx[label]])
+    print('distance: ', distance)
 
-# read video
-videoPath = '../videos/raw/IMG_7378.MOV'
-capture = cv2.VideoCapture(videoPath)
+knn_judge(dist, ind, image_labels)
 
-if not capture.isOpened():
-    print("could not open :", videoPath)
-else:
-    totalFrame = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    fps = capture.get(cv2.CAP_PROP_FPS)
-
-    videoFormat = capture.get(cv2.CAP_PROP_FORMAT)
-
-    print('totalFrame: ', totalFrame)
-    print('width: ', width)
-    print('height: ', height)
-    print('fps: ', fps)
-    print('videoFormat: ', videoFormat)
-
-frameIdx = 0
-while True:
-    frameIdx += 1
-    print('frameIdx: ', frameIdx)
-    # grab the current frame
-    (grabbed, frame) = capture.read()
-
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if not grabbed:
-        break
-
-    if frameIdx % 30 != 0:
-        continue
-
-    if frameIdx > 400:
-        break
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    fm = variance_of_laplacian(gray)
-
-    # detect blur
-    if fm < 400:
-        continue
-
-    frame = imutils.resize(frame, width=600)
-    cv2.imshow('frame', frame)
-    ck = cv2.waitKey(30) & 0xFF
-    if ck == 27:
-        break
+# from collections import Counter
+# import numpy as np
+# x = ['c','a','b','c','a','d']
+#
+#
+# count = Counter(x)
+# print(count)
+#
+# print(count.most_common())
+#
+#
+# a = np.array(['a', 'b', 'a',  'b', 'c', 'c', 'b', 'c', 'a'])
+# # a = ['a', 'b', 'a',  'b', 'c', 'c', 'b', 'c', 'a']
+#
+# b = [1, 2, 2, 3, 3, 3, 3, 3, 4, 5, 6, 6, 6]
+#
+# result = Counter(a.tolist()).most_common()
+#
+# print('result: ', result)
+#
+# print(result[0][0])
+# print(result[1][0])
+# print(Counter(b).most_common())
+#
+#
+#
+# c = np.array([0, 2, 1])
+# d = np.array(['a', 'b', 'c'])
+#
+# e = d[c.tolist()]
+#
+# print(e)
+# print(type(e))
+#
 
 
-capture.release()
-cv2.destroyAllWindows()
-print('hello world')
+
+# from scipy import misc
+# import cv2
+#
+#
+# image = misc.imread('/Users/zhp/Project/facenet/faceDataset/test/0/0.png')
+# image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+# cv2.imshow('name', image)
+# ck = cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+
+# import cv2
+# from scipy import misc
+# import imutils
+#
+#
+# def save_image(img, faceRectangles, imgPath):
+#     # # Draw a rectangle around the faces
+#     for faceIdx, (left, top, right, bottom, p) in enumerate(faceRectangles):
+#         cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 2)
+#
+#         font = cv2.FONT_HERSHEY_SIMPLEX
+#         cv2.putText(img, 'faceIdx: {}'.format(faceIdx), (int(left), int(top)), font, 1, (0, 0, 255), 1, cv2.LINE_AA)
+#     misc.imsave(imgPath, img)
+#     return
+#
+#
+# def variance_of_laplacian(image):
+#     # compute the Laplacian of the image and then return the focus
+#     #  measure, which is simply the variance of the Laplacian
+#     return cv2.Laplacian(image, cv2.CV_64F).var()
+#
+#
+# # read video
+# videoPath = '../videos/raw/IMG_7378.MOV'
+# capture = cv2.VideoCapture(videoPath)
+#
+# if not capture.isOpened():
+#     print("could not open :", videoPath)
+# else:
+#     totalFrame = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+#     width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+#     height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+#     fps = capture.get(cv2.CAP_PROP_FPS)
+#
+#     videoFormat = capture.get(cv2.CAP_PROP_FORMAT)
+#
+#     print('totalFrame: ', totalFrame)
+#     print('width: ', width)
+#     print('height: ', height)
+#     print('fps: ', fps)
+#     print('videoFormat: ', videoFormat)
+#
+# frameIdx = 0
+# while True:
+#     frameIdx += 1
+#     print('frameIdx: ', frameIdx)
+#     # grab the current frame
+#     (grabbed, frame) = capture.read()
+#
+#     # if we are viewing a video and we did not grab a frame,
+#     # then we have reached the end of the video
+#     if not grabbed:
+#         break
+#
+#     if frameIdx % 30 != 0:
+#         continue
+#
+#     if frameIdx > 400:
+#         break
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#
+#     fm = variance_of_laplacian(gray)
+#
+#     # detect blur
+#     if fm < 400:
+#         continue
+#
+#     frame = imutils.resize(frame, width=600)
+#     cv2.imshow('frame', frame)
+#     ck = cv2.waitKey(30) & 0xFF
+#     if ck == 27:
+#         break
+#
+#
+# capture.release()
+# cv2.destroyAllWindows()
+# print('hello world')
 
 
 
